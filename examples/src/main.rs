138b4f27e0c3934dec7f83820c568560f8a3f01d
wada <wada@avita.co.jp>
2023-04-04 13:00:50 +0900

    execute `cargo fmt`

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,7 +1,7 @@
-use sum::sum;
 use subtract::subtract;
+use sum::sum;
 
 fn main() {
-  println!("2 + 2 = {}", sum(2, 2));
-  println!("2 - 2 = {}", subtract(2, 2));
+    println!("2 + 2 = {}", sum(2, 2));
+    println!("2 - 2 = {}", subtract(2, 2));
 }
