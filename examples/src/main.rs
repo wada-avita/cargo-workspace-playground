17b6e4c1af7bfc9185a051f6bff1f08c6e45c28a
wada-avita <102776513+wada-avita@users.noreply.github.com>
2023-04-04 14:44:58 +0900

    add workflow for CI (#2)
    * add workflow for CI
    
    * execute `cargo fmt`
    
    * sort deps
    
    * add doc test, update CI
    
    * add doc test
    
    * update Cargo.toml, CI
    
    * fix for CI
    
    * update CI
    
    * update CI
    
    * update MSRV

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -1,7 +1,15 @@
-use sum::sum;
+//! This is a simple example of how to use the `sum` and `subtract` functions
+//!
+//! Run with
+//!
+//! ```not_rust
+//! cargo run -p workspace-examples
+//! ```
+
 use subtract::subtract;
+use sum::sum;
 
 fn main() {
-  println!("2 + 2 = {}", sum(2, 2));
-  println!("2 - 2 = {}", subtract(2, 2));
+    println!("2 + 2 = {}", sum(2, 2));
+    println!("2 - 2 = {}", subtract(2, 2));
 }
--- subtract/src/lib.rs
+++ subtract/src/lib.rs
@@ -1,3 +1,9 @@
+/// 符号付き整数の引き算を行う
+/// # Examples
+/// ```
+/// use subtract::subtract;
+/// assert_eq!(subtract(2, 2), 0);
+/// ```
 pub fn subtract(a: i32, b: i32) -> i32 {
     a - b
 }
